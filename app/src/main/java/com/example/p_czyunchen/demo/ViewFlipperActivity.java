b65b50d31aff1dcaa39eadc7fe92a82906de58aa
ihdoB <1073183792@qq.com>
2019-01-01 13:37:38 +0800

    commit

--- app/src/main/java/com/example/p_czyunchen/demo/ViewFlipperActivity.java
+++ app/src/main/java/com/example/p_czyunchen/demo/ViewFlipperActivity.java
@@ -2,46 +2,83 @@ package com.example.p_czyunchen.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
+import com.bumptech.glide.Glide;
 import com.example.p_czyunchen.demo.bean.News;
+import com.example.p_czyunchen.demo.bean.ProjectChapter;
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+import com.zhy.http.okhttp.utils.L;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Request;
+
 public class ViewFlipperActivity extends Activity {
 
+    String url = "http://wanandroid.com/project/tree/json";
+    List<ProjectChapter.DataBean> beanList = new ArrayList<>();
+    TextView content_tv;
+    ViewFlipper viewFlipper;
+    LinearLayout item_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_flipper);
-
-        ViewFlipper viewFlipper  = findViewById(R.id.flipper);
-        viewFlipper.addView(View.inflate(this,R.layout.item_viewflipper,null));
-
-        viewFlipper.setOnClickListener(v ->
-                Toast.makeText(this, "click viewFlipper.", Toast.LENGTH_SHORT).show());
-
-        TextView content_tv = findViewById(R.id.viewflipper_content);
-        List<News> list = new ArrayList<>(); //存储来自网络的数据
-        list.add(new News("name1","link"));
-        list.add(new News("name2","link"));
-        list.add(new News("name3","link"));
-        list.add(new News("name4","link"));
-        list.add(new News("name5","link"));
-        list.add(new News("name6","link"));
-
-        for(News t : list){
-            content_tv.setText(t.getName());
-            viewFlipper.getCurrentView().setOnClickListener(v ->
-                    Toast.makeText(this, "t.getLink() 跳转WebView  当前是第"+(list.indexOf(t)+1)+"个", Toast.LENGTH_SHORT).show());
-
-            if(list.indexOf(t)+1 == list.size()) Toast.makeText(this, "这是最后一个", Toast.LENGTH_SHORT).show();
-        }
-
-
+        viewFlipper = findViewById(R.id.flipper);
+        ImageView flipper_image = findViewById(R.id.flipper_image);
+        Glide.with(this).load(R.drawable.icon_news).into(flipper_image);
+        getData();
     }
+
+    private void setData() {
+        for (ProjectChapter.DataBean t : beanList) {
+            item_view = (LinearLayout) LayoutInflater.from(this).inflate( R.layout.item_viewflipper, null);
+            content_tv = item_view.findViewById(R.id.viewflipper_content);
+            content_tv.setText(t.getName());
+            Log.v("jankin_chapter", t.getName() );
+            content_tv.setOnClickListener(v ->
+                    Toast.makeText(this, t.getName() + (beanList.indexOf(t) + 1) + "个", Toast.LENGTH_SHORT).show());
+            viewFlipper.addView(item_view);
+        }
+    }
+
+    private void getData() {
+        OkHttpUtils
+                .get()
+                .url(url)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onBefore(Request request, int id) {
+                        super.onBefore(request, id);
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+
+                        Log.v("flipper ", response);
+                        Gson gson = new Gson();
+                        beanList = gson.fromJson(response, ProjectChapter.class).getData();
+                        setData();
+                    }
+                });
+    }
+
 }
